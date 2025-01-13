f3f08e82b9581ad0d22f47466066fce013407a40
Mifta Chowdhury <mifta.chowdhury@gmail.com>
2025-01-12 22:17:08 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,6 +10,8 @@ var map = new mapboxgl.Map({
   zoom: initialZoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 const apiKey = 'AIzaSyC-nbTvlTo7oKHTQIt9aYDOKiy7qIJglgc'
 
 const ssEventsId = '17VEKYHBH4I5eAs7oQyaEbG27n8Euu_xickYszPtnRFE';
@@ -20,9 +22,28 @@ fetch(`https://sheets.googleapis.com/v4/spreadsheets/${ssEventsId}/values/${shee
   .then(data => console.log(data.values))
   .catch(error => console.error('Error:', error));
 
-console.log(branchList)
 
+  // Create and map markers
+  branchList.forEach((item) => {
 
+          oneMarker = new mapboxgl.Marker()
+          .setLngLat([item.long, item.lat])
+          .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+                    .setHTML(`<b>${item.name}</b><br>${item.phone}`))
+          .addTo(map);
+         currentMarkers.push(oneMarker);
 
+        // Fit bounds after load (i.e. after two clicks 1) Bronx and 2) Hospital)
+        // if (counter>2) {
+        //   var bounds = new mapboxgl.LngLatBounds();
+        //   arrDisp.forEach(function(item) {
+        //     bounds.extend([item.long, item.lat]);
+        //   });
+        //   map.fitBounds(bounds, {padding: {top: 50, bottom: 50, left: 50, right: 50}});
+        // }
+        // counter += 1;
 
-map.addControl(new mapboxgl.NavigationControl());
+        // For early epxloration
+        console.log(oneMarker);
+        console.log(oneMarker["_lngLat"]);
+      });
