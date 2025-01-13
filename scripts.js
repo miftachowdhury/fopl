301667c69e39103a8552ce13d15f242bad2a4850
Mifta Chowdhury <mifta.chowdhury@gmail.com>
2025-01-12 23:31:05 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibWNob3dkaHVyeSIsImEiOiJjazZzdHJta2swNzN2M2tyeHBmZTcycTI4In0.StlNQAWNUjcDoPBeZyIvGw';
 
-var initialCenterPoint = [ -122.230, 37.805]
+var initialCenterPoint = [ -122.2, 37.8]
 var initialZoom = 11.5
 
 
